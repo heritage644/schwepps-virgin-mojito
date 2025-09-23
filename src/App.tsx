81ed97977a83b2a675b61900735126c9d7312be4
Chukwudi Okechukwu <okjohn644@gmail.com>
2025-09-23 23:18:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import NavBar from './components/navbar'
+import './App.css'
+import Hero from './components/hero'
+function App() {
+  return(
+    <>
+    <NavBar/>
+    <Hero/>
+    <div className='h-[100vh]'></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.tsx
@@ -0,0 +1,42 @@
+import { NavLinks } from "../constants"
+import {useGSAP} from "@gsap/react"
+import gsap from "gsap"
+export default function NavBar (){
+useGSAP(()=>{
+    const navTween = gsap.timeline({
+        scrollTrigger:{
+            trigger: 'nav',
+            start:'bottom top'
+        }
+    });
+
+    navTween.fromTo('nav',{ backgroundColor:'transparent'},{
+            backgroundColor:'#00000050',
+            backgroundFilter:'blur(10px)',
+            duration:1,
+            ease:'power1.inOut'
+        
+
+    });
+})
+
+    return(
+    <>
+    <nav >
+        <div >
+            <a href="#home"  className="gap-2 flex items-center">
+                <img src="/images/logo.png" alt="logo" />
+                <p>Velvet Pour</p>
+            </a>
+            <ul>
+               {NavLinks.map((Navs) =>(
+                <li key={Navs.id}>{Navs.title}</li>
+               ))}
+            </ul>
+        </div>
+    </nav>
+    
+    
+    </>
+    )
+}
